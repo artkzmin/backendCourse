1f555045e692ca9bdd8ff2723a54f9dfabe54e6e
artkzmin <artkzmin@gmail.com>
2024-12-19 12:30:26 +0300

    Решение: Вернуть пагинацию и фильтрацию в получение отелей

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -48,23 +48,24 @@ class HotelsRepository(BaseRepository):
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
         )
 
-        hotels_ids_filtered = (
-            select(HotelsOrm.id)
+        query = (
+            select(HotelsOrm)
             .select_from(HotelsOrm)
             .filter(HotelsOrm.id.in_(hotels_ids_to_get))
         )
 
         if location:
-            hotels_ids_filtered = hotels_ids_filtered.filter(func.lower(
+            query = query.filter(func.lower(
                 HotelsOrm.location).contains(location.strip().lower()))
         if title:
-            hotels_ids_filtered = hotels_ids_filtered.filter(func.lower(
+            query = query.filter(func.lower(
                 HotelsOrm.title).contains(title.strip().lower()))
 
-        hotels_ids_filtered = (
-            hotels_ids_filtered
+        query = (
+            query
             .limit(limit)
             .offset(offset)
         )
 
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_filtered))
+        result = await self.session.execute(query)
+        return [Hotel.model_validate(model, from_attributes=True) for model in result.scalars().all()]
